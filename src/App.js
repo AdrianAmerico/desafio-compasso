f10b87ba5da4518946390a0a2fb3c8bc279f954d
Adrian Américo <1fnadrian1@gmail.com>
2021-06-25 18:17:49 -0300

    adicionado login utilizando o github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { githubProvider } from './config/authMethod';
+import githubAuth from './service/auth';
 
 function App() {
+  const handleOnClick = async (provider) => {
+    const res = await githubAuth(provider)
+    console.log(res)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleOnClick(githubProvider)}>Github</button>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+
+export const API = {
+    BASE_URL: "https://api.github.com",
+    CLIENT_SECRET: "2dee49900e714d22255ade47fad7396e871f4c0f",
+    CLIENT_ID: "934ff5ccd781d3d08860"
+}
+
